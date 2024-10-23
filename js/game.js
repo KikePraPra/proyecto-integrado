eb61957a76fe9c3b82373bec043f1c707eca0689
KikePraPra <josezumbado02@gmail.com>
2024-10-22 19:33:14 -0600

    La primera entrega parece estar terminada

--- js/game.js
+++ js/game.js
@@ -1,34 +1,30 @@
-const config = {
-    type: Phaser.AUTO,
-    width: window.innerWidth,
-    height: window.innerHeight,
-    physics: {
-        default: 'arcade',
-    },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
-};
+let gameScene = new Phaser.Scene("Game");
 
-const game = new Phaser.Game(config);
+gameScene.preload = function() {
 
-function preload() {
-
+    //se cargan las imagenes
     this.load.image('background', 'img/assets/fondo-juego.png');
-    this.load.image('player', 'img/assets/principal.png');
     this.load.image('ground1', 'img/assets/ground1.png');
     this.load.image('ground2', 'img/assets/ground2.png');
     this.load.image('enemy1', 'img/assets/enemigo1.png');
     this.load.image('power-up', 'img/assets/power-up.png');
+
+    //se cargan los spritesheets
+    this.load.spritesheet('player', './img/assets/principal-spritesheet.png', {
+        frameWidth: 90,
+        frameHeight: 156,
+        margin: 4,
+        spacing: -18.5
+      });
 }
 
-function create() {
+gameScene.create = function() {
+
+    //se crea una primera versión del escenario
 
     if(window.innerWidth >= 1600) {
         this.add.image(config.width / 2, config.height / 2, 'background').setScale(1.40);
-        this.add.image(150, 500, 'player').depth = 2;
+        this.player = this.add.sprite(150, 500, 'player', 2).depth = 2;
         this.add.image(270, 700, 'ground1').depth = 1;
         this.add.image(1200, 750, 'ground2').depth = 1;
         this.add.image(1200, 510, 'enemy1').depth = 2;
@@ -43,8 +39,27 @@ function create() {
         this.add.image(500, 250, 'enemy1').setScale(0.5).depth = 2;
         this.add.image(670, 195, 'power-up').setScale(0.2).depth = 2;
     }
+ 
 }
 
-function update() {
+gameScene.update = function(){
 
 }
+
+//configuración de la escena
+let config = {
+    type: Phaser.CANVAS,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    scene: gameScene,
+    title: "Gehenna",
+    physics: {
+      default: "arcade",
+      arcade: {
+        gravity: { y: 900 },
+        debug: false,
+      },
+    },
+  };
+
+  let game = new Phaser.Game(config);
